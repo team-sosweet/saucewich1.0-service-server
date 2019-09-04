5a8df784ac9acaefe347e99ff75536a1d0d63aeb
huewilliams <4879sinwoo@naver.com>
2019-09-04 23:52:09 +0900

    Add: app.js, package.json
    app.js : server listening, error handling, import package 
    package.json, package-lock.json : 의존성 관리

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config({path: '.env'});
+
+const app = express();
+
+app.set('port', process.env.PORT || 5000);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// don't match any router
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use((err, req, res) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 500);
+});
+
+// server listening
+app.listen(app.get('port'), () => {
+    console.log(`server listening on ${app.get('port')} port.`);
+});
