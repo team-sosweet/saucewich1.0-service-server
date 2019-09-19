31fe5a9fb2f7843bb447254e8350a8ddce7d31c4
huewilliams <4879sinwoo@naver.com>
2019-09-19 14:52:41 +0900

    FIX: 주석 추가 및 삭제

--- src/routes/util.js
+++ src/routes/util.js
@@ -12,16 +12,8 @@ exports.getAllKeys = function() {
     }))
 };
 
+// 모든 게임 리스트(게임 참가 인원 포함)
 exports.getAllData = function(key) {
-    // return new Promise((resolve, reject) => {
-    //     redisClient.hgetall(key, (err, data) => {
-    //         if(err) {
-    //             reject(err);
-    //         } else {
-    //             resolve(data);
-    //         }
-    //     })
-    // })
     return new Promise((resolve, reject) => {
         redisClient.zrevrange(key, 0, -1, "withscores", (err, data) => {
             if (err) {
@@ -33,16 +25,8 @@ exports.getAllData = function(key) {
     })
 };
 
+// 모든 게임 리스트(게임 코드만)
 exports.getAllGame = function(key) {
-    // return new Promise((resolve, reject) => {
-    //     redisClient.hgetall(key, (err, data) => {
-    //         if(err) {
-    //             reject(err);
-    //         } else {
-    //             resolve(data);
-    //         }
-    //     })
-    // })
     return new Promise((resolve, reject) => {
         redisClient.zrevrange(key, 0, -1, (err, data) => {
             if (err) {
@@ -54,6 +38,7 @@ exports.getAllGame = function(key) {
     })
 };
 
+// 특정 게임의 참가 인원 반환
 exports.getPeople = function (key, roomCode) {
     return new Promise((resolve, reject) => {
         redisClient.zscan(key, 0, 'MATCH', roomCode, (err, data) => {
@@ -64,7 +49,19 @@ exports.getPeople = function (key, roomCode) {
             }
         })
     })
-}
+};
+
+exports.getWaitGame = function (key, people) {
+    return new Promise((resolve, reject) => {
+        redisClient.zrevrangebyscore(key, people, people, (err, data)=>{
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        })
+    })
+};
 
 exports.getValue = function (key, field) {
     return new Promise(((resolve, reject) => {
