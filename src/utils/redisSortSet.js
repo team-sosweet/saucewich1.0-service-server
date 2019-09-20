664e6511e080cbabe0bc98ce2d4b8e612b3f02e4
huewilliams <4879sinwoo@naver.com>
2019-09-20 21:23:08 +0900

    ADD: redis sorted set 함수

---
+++ src/utils/redisSortSet.js
@@ -0,0 +1,64 @@
+const redisClient = require('../models/redisClient');
+
+// 모든 게임 리스트(게임 참가 인원 포함)
+exports.getAllData = function(key) {
+    return new Promise((resolve, reject) => {
+        redisClient.zrevrange(key, 0, -1, "withscores", (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        })
+    })
+};
+
+// 모든 게임 리스트(게임 코드만)
+exports.getAllGame = function(key) {
+    return new Promise((resolve, reject) => {
+        redisClient.zrevrange(key, 0, -1, (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        })
+    })
+};
+
+// 특정 게임의 참가 인원 반환
+exports.getPeople = function (key, roomCode) {
+    return new Promise((resolve, reject) => {
+        redisClient.zscan(key, 0, 'MATCH', roomCode, (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        })
+    })
+};
+
+exports.changePeople = function (key, roomCode, people) {
+    return new Promise(((resolve, reject) => {
+        redisClient.zincrby(key, people, roomCode, (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    }))
+};
+
+exports.getWaitGame = function (key, people) {
+    return new Promise((resolve, reject) => {
+        redisClient.zrevrangebyscore(key, people, people, (err, data)=>{
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        })
+    })
+};
