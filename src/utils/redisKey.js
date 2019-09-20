f987e559f54f3913c08186d5626fafb689c2508f
huewilliams <4879sinwoo@naver.com>
2019-09-20 21:24:14 +0900

    ADD: redis key 함수

---
+++ src/utils/redisKey.js
@@ -0,0 +1,26 @@
+const redisClient = require('../models/redisClient');
+
+// 모든 redis Key 반환
+exports.getAllKeys = function() {
+    return new Promise(((resolve, reject) => {
+        redisClient.keys('*', (err, keys) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(keys);
+            }
+        })
+    }))
+};
+
+exports.deleteKey = function(key) {
+    return new Promise(((resolve, reject) => {
+        redisClient.del(key, (err, data) => {
+            if(err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        })
+    }))
+};
