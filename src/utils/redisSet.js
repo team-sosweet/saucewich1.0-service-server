ccadcb5d14e6349b152188f50b22f02cfd8b35c1
huewilliams <4879sinwoo@naver.com>
2019-09-20 21:23:53 +0900

    FIX: 주석 추가

--- src/utils/redisSet.js
+++ src/utils/redisSet.js
@@ -1,5 +1,6 @@
 const redisClient = require('../models/redisClient');
 
+// 게임 대기 port 추가
 exports.getPort = function (key) {
     return new Promise(((resolve, reject) => {
         redisClient.smembers(key, (err, set)=>{
@@ -12,6 +13,7 @@ exports.getPort = function (key) {
     }))
 }
 
+// 게임 특정 대기 port 삭제
 exports.popPort = function(key, port) {
     return new Promise((resolve, reject)=>{
         redisClient.srem(key, port, (err, set)=>{
